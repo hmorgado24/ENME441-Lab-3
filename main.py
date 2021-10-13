4657f46c0919ee0b64680e2204b711705031e87b
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-10-13 16:54:52 +0000

    fds

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ class Joystick:
     self.adc = PCF8591(address)
   
   def getX(self):
-    return self.string.adc.read(0x40)
+    return str(self.string.adc.read(0x40))
   
   def getY(self):
-    return self.string.adc.read(0x41)
+    return str(self.adc.read(0x41))
 
 while True:
   joystick = Joystick(0x48)
