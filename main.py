46ed2c07ecdf71a95ee59fd41ab44f46204c29e4
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-10-13 16:45:21 +0000

    ur mom

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ class PCF8591:
 
   bus = smbus.SMBus(1)
 
-  def __init__(self,address):
+  def __init__(self, address):
     self.bus = smbus.SMBus(1)
     self.address = address
 
   def read(self, chn): #channel
-      try:
-          self.bus.write_byte(self.address, 0x40 | chn)  # 01000000
-          self.bus.read_byte(self.address) # dummy read to start conversion
-      except Exception as e:
-          print ("Address: %s \n%s" % (self.address,e))
-      return self.bus.read_byte(self.address)
+    try:
+        self.bus.write_byte(self.address, 0x40 | chn)  # 01000000
+        self.bus.read_byte(self.address) # dummy read to start conversion
+    except Exception as e:
+        print ("Address: %s \n%s" % (self.address,e))
+    return self.bus.read_byte(self.address)
 
 class Joystick:
-  
+
   def __init__(self, address):
     self.adc = PCF8591(address)
   
@@ -30,5 +30,4 @@ class Joystick:
 
 while True:
   joystick = Joystick(0x48)
-  print(joystick.getX())
-  print(joystick.getY())
+  print(joystick.getX() + ', ' + joystick.getY())
